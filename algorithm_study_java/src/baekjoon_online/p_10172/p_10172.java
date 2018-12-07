26067335a68a2e5b3f49bfc136765988e5795f4a
Dukwon Nam <ndukwon@gmail.com>
2018-12-07 22:57:09 +0900

    [baekjoon] p_10172

---
+++ algorithm_study_java/src/baekjoon_online/p_10172/p_10172.java
@@ -0,0 +1,24 @@
+package baekjoon_online.p_10172;
+
+/*
+
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+
+ */
+public class p_10172 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|\n");
+        sb.append("|q p|   /}\n");
+        sb.append("( 0 )\"\"\"\\\n");
+        sb.append("|\"^\"`    |\n");
+        sb.append("||_/=\\\\__|\n");
+
+        System.out.print(sb);
+    }
+}
